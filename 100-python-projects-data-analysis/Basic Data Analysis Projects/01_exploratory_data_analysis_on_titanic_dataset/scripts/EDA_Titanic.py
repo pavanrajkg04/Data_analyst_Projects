32b0e139634fca0ec7c3ecadc5092d5905bb88ef
Pavan Raj K G <nesarastudio0492@gmail.com>
2024-07-02 12:36:05 +0530

    1

--- 100-python-projects-data-analysis/Basic Data Analysis Projects/01_exploratory_data_analysis_on_titanic_dataset/scripts/EDA_Titanic.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-
-
-Test_data = pd.read_csv(r'100-python-projects-data-analysis\Basic Data Analysis Projects\01_exploratory_data_analysis_on_titanic_dataset\datasets\test.csv')
-Train_data = pd.read_csv(r'100-python-projects-data-analysis\Basic Data Analysis Projects\01_exploratory_data_analysis_on_titanic_dataset\datasets\train.csv')
-print(Train_data.head(5))
